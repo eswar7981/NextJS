6ed58372ea373c4f74568229b46e5ffd9dfb3d16
eswar7396 <eswarsatyavarapu7981@gmail.com>
2023-12-08 16:43:41 +0530

    response based on the routes

--- firstResponse.js
+++ firstResponse.js
@@ -1,13 +1,31 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write(
-    "<header><title>First</title></header><body><p>My first response</p></body>"
-  );
-  res.write("</html>");
-  res.end()
+  const url = req.url;
+  if (url === "/home") {
+    res.write("<html>");
+    res.write(
+      "<header><title>First</title></header><body><p>Welcome Home</p></body>"
+    );
+    res.write("</html>");
+    res.end();
+  } else if (url === "/about") {
+    res.write("<html>");
+    res.write(
+      "<header><title>First</title></header><body><p>Welcome to About us Page</p></body>"
+    );
+    res.write("</html>");
+    res.end();
+  } else if (url === "/node") {
+    res.write("<html>");
+    res.write(
+      "<header><title>First</title></header><body><p>Welcome to my Node.js project</p></body>"
+    );
+    res.write("</html>");
+    res.end();
+  }
+
+  res.end();
 });
 
 server.listen(3000);
