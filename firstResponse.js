40338669a445fd63259c460c8007779038f987df
eswar7396 <eswarsatyavarapu7981@gmail.com>
2023-12-13 14:51:25 +0530

    new data will be added into the text document

--- firstResponse.js
+++ firstResponse.js
@@ -1,31 +1,54 @@
 const http = require("http");
-
+const fs = require("fs");
 const server = http.createServer((req, res) => {
   const url = req.url;
-  if (url === "/home") {
-    res.write("<html>");
-    res.write(
-      "<header><title>First</title></header><body><p>Welcome Home</p></body>"
-    );
+  var fetched=['d','dd']
+  
+
+
+  if (url === "/") {
+    res.write('<html>')
+    res.write('<head><title>First</title></head>')
+    res.write('<body>')
+    fs.readFile("./message.txt",(err,data)=>{
+      if(err){
+        console.log(err)
+      }
+      else{
+      fetched=[...fetched,...data.toString().split(',')]
+   
+      }
+    } 
+    )
+    fetched.map((elem)=>{
+      res.write('hello')
+    })
+    res.write('<p>Enter Details Here</p><?php fread($myfile,filesize("message.txt"))  ?><form action="/message" method="POST"><input type="text" name="message" ></input><button type="submit">Send</button></form>')
+    res.write('</body>')
     res.write("</html>");
-    res.end();
-  } else if (url === "/about") {
-    res.write("<html>");
-    res.write(
-      "<header><title>First</title></header><body><p>Welcome to About us Page</p></body>"
-    );
-    res.write("</html>");
-    res.end();
-  } else if (url === "/node") {
-    res.write("<html>");
-    res.write(
-      "<header><title>First</title></header><body><p>Welcome to my Node.js project</p></body>"
-    );
-    res.write("</html>");
-    res.end();
+
+  
+  res.end()}
+  if (url === "/message") {
+    const body=[];
+    req.on('data',(chunk)=>{
+      body.push(chunk)
+    })
+    req.on('end',()=>{
+      const parseBody=Buffer.concat(body).toString()
+      const data=parseBody.split('=')
+   
+      fs.appendFile("message.txt",data[1]+',',err=>{
+        res.statusCode = 302;
+        res.setHeader("location", "/");
+        return res.end();
+      });
+      
+    })
+   
   }
 
-  res.end();
+
 });
 
 server.listen(3000);
